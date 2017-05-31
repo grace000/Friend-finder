8a47f9027aaa4f8174393b58e28706b7bb1dea5a
grace000 <tiffani.grace.elmore@gmail.com>
2017-05-30 20:35:12 -0500

    updated match posting

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -24,15 +24,44 @@ module.exports = function(app) {
   // API POST Requests
   // ---------------------------------------------------------------------------
 
-  app.post("/api/friends", function(req, res) {
-    if (friendData.length < 5) {
-      friendData.push(req.body);
-      res.json(true);
+  app.post('/api/friends', function (req, res) {
+    //Note the code here. Our "server" will respond to request and let users know if they have a table or not.
+    
+    var currentUserScores = req.body.scores;
+    var lowestDifference = 80;
+    var difference = 0;
+    var bestMatch = friendData[0];
+
+    for(var i = 0; i < friendData.length; i++) {
+      var possibleMatchScores = friendata[i].scores;
+      for(var x = 0; x < possibleMatchScores.length; x++) {
+        difference += Math.abs(currentUserScores[x] - possibleMatchScores[x]);
+      }
+
+      if (difference <= lowestDifference) {
+        lowestDifference = difference;
+        bestMatch = surveyData[i];
+      }
+
+        difference = 0;
+
     }
-    else {
-      res.json(false);
-    }
+
+  friendData.push(req.body);
+
+  res.json(bestMatch); 
+
+
+
   });
+
+  app.post('/api/clear', function() {
+    //Empty out the arrays data
+    surveyArray = [];
+
+  });
+ 
+}
 
   // ---------------------------------------------------------------------------
 

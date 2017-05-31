c2f7146b5b6582350504cb6e699055f09be66a50
grace000 <tiffani.grace.elmore@gmail.com>
2017-05-30 20:19:20 -0500

    updated data pull requirement

--- server.js
+++ server.js
@@ -7,16 +7,8 @@ var path = require("path");
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = 3306;
+var PORT =  process.env.PORT || 3306;
 
-// // ==Express Setup==
-// const app = express();
-// app.set('port', (process.env.PORT || 5000));
-
-// //Start Server
-// app.listen(app.get('port'), function() {
-//   console.log('Node app is running on port', app.get('port'));
-// });
 
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.json());
@@ -27,18 +19,8 @@ app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 //  (DATA)
 // =============================================================
 
-// Routes
-// =============================================================
-
-// Basic route that sends the user first to the AJAX Page
-app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "home.html"));
-});
-
-app.get("/add", function(req, res) {
-  res.sendFile(path.join(__dirname, "friends.html"));
-});
-
+require('./app/routing/api-routes.js')(app);
+require('./app/routing/html-routes.js')(app);
 
 // Starts the server to begin listening
 // =============================================================
